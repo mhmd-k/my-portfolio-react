052ff65b0fc4a54c54d25d14e4736927d800dd21
mhmd-k <mhmdk425@gmail.com>
2023-09-25 19:10:24 +0300

    trying to fix not click in mobile

--- src/components/AnimationDiv.js
+++ src/components/AnimationDiv.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useThemeContext } from "../context/ThemeContext";
 
 function AnimationDiv() {
   const { theme } = useThemeContext();
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setShow(false), 2200);
+  }, []);
+
+  useEffect(() => {
+    setShow(true);
+    setTimeout(() => setShow(false), 2200);
+  }, [theme]);
+
+  if (!show) {
+    return;
+  }
 
   return (
     <>
